e3156c6d88d7347c4a2cb634fcdfc76d803bb20b
Aehithishyam Palagiri <aehithishyam3316@gmail.com>
2026-01-12 13:11:15 +0530

    loading state component with spinner
    Add loading state component with spinner

---
+++ src/modules/admin/components/LoadingState.tsx
@@ -0,0 +1,10 @@
+function LoadingState() {
+  return (
+    <div className="loading-state">
+      <div className="loading-spinner"></div>
+      <p className="loading-text">Loading....</p>
+    </div>
+  );
+}
+
+export default LoadingState;
